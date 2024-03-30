d7b509fe9ba48403a1480178d2ae37ebf4209626
Einsteinia11 <informationviakate2004@gmail.com>
2024-03-30 15:42:08 +0530

    added the first prolog program

---
+++ first.pl
@@ -0,0 +1,7 @@
+%Write a program to calculate the sum of two numbers
+sum(X, Y, Result):-
+    Result is X+Y.
+
+% To run the above code compile buffer then type the function name on
+% the console like shown below
+% sum(3,4, Result).
